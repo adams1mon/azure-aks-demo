f5f1498d05b7dc9ce4cab783b235ac0bbf557a08
Adam Simon <adamsimon85100@gmail.com>
2023-03-18 18:56:26 +0200

    gitignore changed

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 resource "local_file" "kubeconfig" {
   depends_on   = [azurerm_kubernetes_cluster.cluster]
-  filename     = "kubeconfig"
+  filename     = "${path.root}/confidential/kubeconfig"
   content      = azurerm_kubernetes_cluster.cluster.kube_config_raw
 }
 
-resource "null" "export_kubeconfig_env" {
+resource "null_resource" "export_kubeconfig_env" {
   
   depends_on = [
     local_file.kubeconfig
   ]
 
   provisioner "local-exec" {
-    command = "export KUBECONFIG=$${PWD}/kubeconfig"
+    command = "export KUBECONFIG=${path.root}/confidential/kubeconfig"
   }
 }
