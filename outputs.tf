4d7d6b0dd20006acb86c0175385a2d48d3d682e4
Adam Simon <adamsimon85100@gmail.com>
2023-03-19 17:36:16 +0200

    removed setting kubeconfig env var because it doesn't work (subshell)

--- outputs.tf
+++ outputs.tf
@@ -3,14 +3,3 @@ resource "local_file" "kubeconfig" {
   filename     = "${path.root}/confidential/kubeconfig"
   content      = azurerm_kubernetes_cluster.cluster.kube_config_raw
 }
-
-resource "null_resource" "export_kubeconfig_env" {
-  
-  depends_on = [
-    local_file.kubeconfig
-  ]
-
-  provisioner "local-exec" {
-    command = "export KUBECONFIG=${path.root}/confidential/kubeconfig"
-  }
-}
